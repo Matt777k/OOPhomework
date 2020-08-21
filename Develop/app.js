8d04305ef58d86278c7530c867376175caf7b50b
Matthew Kerr <matt777k@gmail.com>
2020-08-21 19:56:16 -0400

    started on the classes. Emplyee class is mostly done. Also built some of the prompy messages

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,7 +10,23 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "what is your first and last name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "what is your id?",  
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is your email address?", 
+  }
+]);
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+
+// The first class is an Employee parent class with the following properties and methods:
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+getName() {
+console.log(`Name: ${this.name}`);
+}
+getId() {
+    console.log(`ID: ${this.id}`);
+}
+getEmail() {
+    console.log(`Email: ${this.email}`);
+}
+getRole() {
+    // Returns 'Employee'
+    return Employee
+}
+}
+
+module.exports = Employee;
